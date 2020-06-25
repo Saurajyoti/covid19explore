3392fa85192ea89480999a741653d9f10ece2837
Saurajyoti <saurajyoti.kar@gmail.com>
2020-06-25 15:08:21 -0400

    redy to upload

--- census_startup.R
+++
@@ -1,8 +0,0 @@
-#install.packages('tidycensus')
-
-library(tidycensus)
-
-#census_api_key('d3ea46368bad8adc9fd421d286aeb82d8a9f7bfc', install = TRUE)
-#readRenviron("~/.Renviron")
-
-us_pop <- get_estimates(geography = 'county', product = 'population')
