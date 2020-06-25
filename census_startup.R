8ba9b77b034db267c7f43e07c330d088d3824bea
Saurajyoti <saurajyoti.kar@gmail.com>
2020-06-25 15:03:31 -0400

    update

--- census_startup.R
+++ census_startup.R
@@ -3,5 +3,6 @@
 library(tidycensus)
 
 #census_api_key('d3ea46368bad8adc9fd421d286aeb82d8a9f7bfc', install = TRUE)
+#readRenviron("~/.Renviron")
 
 us_pop <- get_estimates(geography = 'county', product = 'population')
